429133e58cf750ca7ef270fe3e8d4fd4f4284313
kyo <sanghunjlee@gmail.com>
2024-01-31 00:54:12 -0800

    Remove react-scripts & Add vite

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 import App from './pages/App';
 import Home from './pages/home'
@@ -58,8 +57,3 @@ root.render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
